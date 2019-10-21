9770bf2b44999fca077465e72ee71e127a668300
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-21 19:37:01 -0400

    vuetify and axios implement

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
