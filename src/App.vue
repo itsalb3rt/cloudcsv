74396a6b6cf4452553d6a70d00728efe8a64619b
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-23 10:07:19 -0400

    add snackbar

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,33 @@
       <v-container grid-list-xs>
         <router-view />
       </v-container>
+      <v-snackbar
+        :value="snackbar.show"
+        :multi-line="snackbar.multiLine"
+        :top="snackbar.top === 'top' "
+        :bottom="snackbar.bottom === 'bottom' "
+        :color="snackbar.color"
+      >
+        {{snackbar.message}}
+        <v-btn dark text @click="setSnackbarShow(false)">Cerrar</v-btn>
+      </v-snackbar>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import { mapState, mapMutations } from "vuex";
+
 export default {
   name: "App",
   data: () => ({
     //
-  })
+  }),
+  methods: {
+    ...mapMutations(["setSnackbar", "setSnackbarShow"])
+  },
+  computed: {
+    ...mapState(["user", "isLoged", "currentNavegationArea", "snackbar"])
+  }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    snackbar: {
+      show: false,
+      message: "",
+      color: "",
+      top: null,
+      bottom: null,
+      multiLine: true,
+    }
   },
   mutations: {
+    setSnackbar(state, snackbar) {
+      state.snackbar.show = snackbar.show;
+      state.snackbar.message = snackbar.message;
+      state.snackbar.color = snackbar.color;
+      state.snackbar.top = (snackbar.top === undefined) ? null : snackbar.top;
+      state.snackbar.bottom = (snackbar.bottom === undefined) ? null : snackbar.bottom;
+      setTimeout(()=>{
+        state.snackbar.show = false;
+      },5000);
+    },
+    setSnackbarShow(state,value){
+      state.snackbar.show = value;
+    },
   },
   actions: {
   },
