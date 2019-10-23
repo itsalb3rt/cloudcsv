bba9ac7defd6e2ddf74e013d409e6d35574b4877
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-23 12:20:03 -0400

    remove timeout

--- src/api/HttpClient.js
+++ src/api/HttpClient.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const httpClient = axios.create({
     baseURL: (process.env.NODE_ENV == 'development') ? process.env.VUE_APP_BASE_URL_DEV : process.env.VUE_APP_BASE_URL_PRO,
-    timeout: 1000, // indicates, 1000ms ie. 1 second
     headers: {
         "Content-Type": "application/json",
     }
