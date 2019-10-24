44c615ac7e141b4bc67d8df778df94de24b6647e
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-23 20:12:33 -0400

    add register

--- src/api/Auth.api.js
+++ src/api/Auth.api.js
@@ -4,6 +4,9 @@ const END_POINT = '/auth/login';
 
 const login = payload => httpClient.post(END_POINT, payload);
 
+const register = payload => httpClient.post('auth/register',payload);
+
 export {
-    login
+    login,
+    register
 }
