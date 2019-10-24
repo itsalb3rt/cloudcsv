feae26f14fa31f5a1204311b690ca2eedad1f5a9
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-23 20:14:30 -0400

    create mixin for forms rules

---
+++ src/mixins/Miscellany/FormRules.js
@@ -0,0 +1,21 @@
+export default {
+    data() {
+        return {
+            rules: {
+                requiredInput: [
+                    v => !!v || this.$t('forms.inputRequired')
+                ],
+                email: [
+                    v => !!v || this.$t('forms.emailIsRequired'),
+                    v => /.+@.+\..+/.test(v) || this.$t('forms.invalidEmail'),
+                ],
+                minLength8:[
+                    v => v.length > 7 || this.$t('forms.minLength8'),
+                ],
+                userName:[
+                    v => /^(?=.{6,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/.test(v) || this.$t('forms.userNameInformation'),
+                ]
+            }
+        }
+    }
+}
