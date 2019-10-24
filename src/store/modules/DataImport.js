4afa8d89a6368a6f0f4a87ff85f04a9572b67027
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-23 23:01:04 -0400

    data import init

---
+++ src/store/modules/DataImport.js
@@ -0,0 +1,29 @@
+import i18n from '@/i18n'
+
+export default {
+    namespaced: true,
+    state: {
+        tables: [],
+        csvDelimiters: [{
+                text: i18n.t('dataPanel.automatic'),
+                value: 'auto'
+            },
+            {
+                text: i18n.t('dataPanel.comma'),
+                value: ','
+            },
+            {
+                text: i18n.t('dataPanel.semicolon'),
+                value: ';'
+            },
+            {
+                text: i18n.t('dataPanel.tabulation'),
+                value: 'tab'
+            },
+            {
+                text: i18n.t('dataPanel.pipe'),
+                value: '|'
+            },
+        ]
+    },
+}
---
+++ src/views/Data/Import.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="5">
+        <v-card elevation="1">
+          <v-card-text>
+            <p>{{$t('dataPanel.importDataInformation')}}</p>
+            <v-select
+              :items="$store.state.dataImport.tables"
+              v-model="importData"
+              outlined
+              :label="$t('dataPanel.tables')"
+            ></v-select>
+            <v-select
+              :items="$store.state.dataImport.csvDelimiters"
+              v-model="csvDelimiter"
+              outlined
+              :label="$t('dataPanel.csvDelimiter')"
+            ></v-select>
+            <div>
+              <v-file-input :label="$t('dataPanel.addCsvLabel')"></v-file-input>
+            </div>
+            <v-divider></v-divider>
+            <div class="mt-4">
+              <v-btn :disabled="importData.length === 0" color="primary">
+                <template
+                  v-if="importData.length === 0"
+                >{{$t('dataPanel.submitButtonTextDisabled')}}</template>
+                <template v-else>
+                  <v-icon class="mr-2">fa-upload</v-icon>
+                  {{$t('dataPanel.submitButtonText')}}
+                </template>
+              </v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="7">
+        <v-card elevation="1" min-height="385">
+          <v-card-text>
+            <template v-if="importData.length === 0">
+              <div class="text-center mt-10">
+                <v-icon color="success" class="mr-2">fa-info-circle</v-icon>
+                <span class="font-weight-bold primary--text">{{$t('dataPanel.beforeImportTableInfo')}}</span>
+              </div>
+            </template>
+            <template v-else>
+              <v-icon class="mr-2">fa-upload</v-icon>
+              {{$t('dataPanel.submitButtonText')}}
+            </template>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      importData: [],
+      csvDelimiter: "auto"
+    };
+  }
+};
+</script>
