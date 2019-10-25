3b2f53983ef972aa949847ba30040bb9712eaf61
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-25 09:13:05 -0400

    minor modifications

--- src/components/TheMenu.vue
+++ src/components/TheMenu.vue
@@ -13,15 +13,27 @@
       </v-toolbar-items>
 
       <template v-if="$vuetify.breakpoint.smAndUp">
-        <v-btn icon>
-          <v-icon>mdi-export-variant</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-delete-circle</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-plus-circle</v-icon>
-        </v-btn>
+        <v-divider class="mx-4" vertical></v-divider>
+
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn color="primary" text v-on="on">
+              <v-avatar size="35">
+                <v-icon dark>fa-user-circle</v-icon>
+              </v-avatar>
+              <span class="ml-2 black--text">{{$store.state.auth.user.user_name}}</span>
+              <v-icon class="ml-2">fa-angle-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item :to="item.route" v-for="(item, index) in profileOptions" :key="index">
+              <v-list-item-title>
+                <v-icon class="mr-2">{{item.icon}}</v-icon>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </template>
     </v-toolbar>
   </div>
@@ -30,6 +42,10 @@
 export default {
   data() {
     return {
+      profileOptions: [
+        { text: "Settings", route: "/settings/profile", icon: "fa-cog" },
+        { text: "Logout", route: "/auth/logout", icon: "fa-sign-out-alt" }
+      ],
       items: [
         { text: "UPLOAD DATA", route: "/data/import" },
         { text: "QUERY", route: "/data/export" },
