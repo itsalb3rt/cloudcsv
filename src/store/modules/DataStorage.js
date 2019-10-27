f9ace4e6f50ac4737e2939b1d34d8754dbf82754
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-27 11:24:37 -0400

    create a data storage module

---
+++ src/store/modules/DataStorage.js
@@ -0,0 +1,36 @@
+import {
+    getDataByTable,
+    save,
+    deleteByTable
+} from "@/api/DataStorage.api"
+
+export default {
+    namespaced: true,
+    state: {
+        items: [],
+    },
+    getters:{
+        getTables(state) {
+            return state.items;
+        }
+    },
+    actions: {
+        saveData({commit}, payload) {
+            return save(payload);
+        },
+        getDataByTable({ commit},idTable) {
+            return getDataByTable(idTable);
+        },
+        deleteRecord({commit},payload){
+            return deleteByTable(payload);
+        }
+    },
+    mutations: {
+        SET_TABLES(state, data) {
+            state.items = data;
+        },
+        REMOVE_TABLE(state,index){
+            state.items.splice(index,1);
+        }
+    }
+}
