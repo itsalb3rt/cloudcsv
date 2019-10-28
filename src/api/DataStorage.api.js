00f5d31023d212ea164487c0944144c0673dcb83
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-27 22:05:49 -0400

    minor modification for accept query string

--- src/api/DataStorage.api.js
+++ src/api/DataStorage.api.js
@@ -2,7 +2,15 @@ import httpClient from './HttpClient';
 
 const END_POINT = '/datastorage/datastorage';
 
-const getDataByTable = id => httpClient.get(END_POINT, id);
+
+/**
+ * The payload is a object like
+ * payload:{
+ * tableId:1,
+ * queryString: '?create_at>=2019-10-01&create_at<=2019-10-30&...'
+ * }
+ */
+const getDataByTable = payload => httpClient.get(`${END_POINT}/${payload.tableId}${payload.queryString}`, payload);
 
 const save = payload => httpClient.post(END_POINT, payload);
 
