89bb0707b14a182ddebec9bd8d7278889a64b61c
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-27 22:06:28 -0400

    data export panel create

---
+++ src/views/Data/Export.vue
@@ -0,0 +1,203 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="4">
+        <v-card elevation="1">
+          <v-card-text>
+            <v-form ref="queryForm">
+              <div class="mb-3">
+                <p class="black--text font-weight-bold">{{$t('dataPanel.exportDataInformation')}}</p>
+                <v-select
+                  :items="$store.getters['tables/getTables']"
+                  v-model="table"
+                  item-text="table_name"
+                  item-value="id_table_storage"
+                  outlined
+                  :label="$t('dataPanel.tables')"
+                ></v-select>
+              </div>
+              <v-divider></v-divider>
+              <div class="mb-3">
+                <v-switch
+                  v-model="showOptions"
+                  color="primary"
+                  :label="`${$t('dataPanel.showOptions')}`"
+                ></v-switch>
+                <div v-if="showOptions">
+                  <p class="black--text font-weight-bold">{{$t('dataPanel.exportDataInformation')}}</p>
+                  <div>
+                    <v-menu
+                      ref="startDateMenu"
+                      v-model="startDateMenu"
+                      :close-on-content-click="false"
+                      :return-value.sync="startDate"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="290px"
+                    >
+                      <template v-slot:activator="{ on }">
+                        <v-text-field
+                          v-model="startDate"
+                          :label="$t('dataPanel.startDate')"
+                          prepend-icon="fa-calendar"
+                          readonly
+                          v-on="on"
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker v-model="startDate" no-title scrollable>
+                        <v-spacer></v-spacer>
+                        <v-btn text color="primary" @click="startDateMenu = false">Cancel</v-btn>
+                        <v-btn
+                          text
+                          color="primary"
+                          @click="$refs.startDateMenu.save(startDate)"
+                        >{{$t('callAction.save')}}</v-btn>
+                      </v-date-picker>
+                    </v-menu>
+                  </div>
+                  <div>
+                    <v-menu
+                      ref="endDateMenu"
+                      v-model="endDateMenu"
+                      :close-on-content-click="false"
+                      :return-value.sync="endDate"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="290px"
+                    >
+                      <template v-slot:activator="{ on }">
+                        <v-text-field
+                          v-model="endDate"
+                          :label="$t('dataPanel.endDate')"
+                          prepend-icon="fa-calendar"
+                          readonly
+                          v-on="on"
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker v-model="endDate" no-title scrollable>
+                        <v-spacer></v-spacer>
+                        <v-btn text color="primary" @click="endDateMenu = false">Cancel</v-btn>
+                        <v-btn
+                          text
+                          color="primary"
+                          @click="$refs.endDateMenu.save(endDate)"
+                        >{{$t('callAction.save')}}</v-btn>
+                      </v-date-picker>
+                    </v-menu>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <v-btn
+                  :loading="loadingQuery"
+                  :disabled="table.length === 0 || !isDatesFill"
+                  color="primary"
+                  @click="query()"
+                >{{$t('callAction.query')}}</v-btn>
+              </div>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="8">
+        <v-card>
+          <v-card-text>
+            <v-data-table
+              :no-data-text="$t('dataPanel.noDataAvailable')"
+              :headers="headers"
+              :items="data"
+              class="elevation-1"
+              :loading="loadingQuery"
+            ></v-data-table>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+import Papa from "papaparse";
+
+export default {
+  mounted() {
+    this.$store
+      .dispatch("tables/fetchAll")
+      .then(response => {
+        this.$store.commit("tables/SET_TABLES", response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  data() {
+    return {
+      table: "",
+      showOptions: false,
+      startDate: "",
+      endDate: "",
+      startDateMenu: false,
+      endDateMenu: false,
+      loadingQuery: false,
+      headers: [],
+      data: []
+    };
+  },
+  methods: {
+    query() {
+      this.loadingQuery = true;
+
+      this.$store
+        .dispatch("dataStorage/getDataByTable", {
+          tableId: this.table,
+          queryString: this.getQueryString()
+        })
+        .then(response => {
+          if (response.status === 200) {
+            if (response.data.length > 0) {
+              this.setHeaders(Object.keys(response.data[0]), ["id_user"]);
+              this.data = response.data;
+            } else {
+              this.data = [];
+              this.headers = [];
+            }
+
+            this.loadingQuery = false;
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getQueryString() {
+      if (this.startDate.length > 0 && this.endDate.length > 0) {
+        return `?create_at>=${this.startDate}&create_at<=${this.endDate}`;
+      } else {
+        return " ";
+      }
+    },
+    setHeaders(headers, omitHeaders = null) {
+      headers.forEach(header => {
+        if (omitHeaders !== null) {
+          if (!omitHeaders.includes(header)) {
+            this.headers.push({ text: header, value: header });
+          }
+        } else {
+          this.headers.push({ text: header, value: header });
+        }
+      });
+    }
+  },
+  computed: {
+    isDatesFill() {
+      if (
+        (this.startDate.length > 0 && this.endDate.length > 0) ||
+        (this.startDate.length === 0 && this.endDate.length === 0)
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
+</script>
