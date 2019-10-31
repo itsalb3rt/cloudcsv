26a8e3b5b3d198982e7e1296adfcd4bb30e8165c
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-30 22:53:25 -0400

    create a user manager panel

---
+++ src/components/Settings/UsersManager.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p class="font-weight-bold">{{$t('settings.userManager')}}</p>
+    <p>{{$t('settings.userManagerInformation')}}</p>
+    <v-btn to="/settings/users" outlined color="primary">
+      <v-icon class="mr-2">fa-user</v-icon>
+      {{$t('settings.userManager')}}
+    </v-btn>
+    <v-switch
+      color="primary"
+      disabled
+      :label="$t('settings.permitNewUserRegister')"
+      v-model="permitNewUserRegister"
+    ></v-switch>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      permitNewUserRegister: true
+    };
+  }
+};
+</script>
