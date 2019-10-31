67c151668cd467de8bd0aa31301fb049c94afd52
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-31 08:51:57 -0400

    minor modification on recovery-account file route

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,7 @@ const routes = [{
   {
     path: '/auth/recovery-account',
     name: 'recovery account',
-    component: () => import('@/views/Auth/RecoveryAccount.vue'),
+    component: () => import('@/views/Auth/ResetPassword.vue'),
     meta: {
       area: 'public',
       authorizationRequired: false,
