c1b4af517bb0c5384b887bceb90a1d0f3228da7a
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-30 22:49:04 -0400

    add method for update user information on admin panel and personal profile

--- src/api/Users.api.js
+++ src/api/Users.api.js
@@ -3,14 +3,16 @@ import httpClient from './HttpClient';
 const END_POINT = '/users/users';
 
 const getAllUsers = () => httpClient.get(END_POINT);
-
 // you can pass arguments to use as request parameters/data
 const getUser = (user_id) => httpClient.get(END_POINT, { user_id });
 // maybe more than one..
 const createUser = (username, password) => httpClient.post(END_POINT, { username, password });
 
+const update = payload => httpClient.patch(`${END_POINT}/${payload.id_user}`,payload);
+
 export {
     getAllUsers,
     getUser,
-    createUser
+    createUser,
+    update
 }
