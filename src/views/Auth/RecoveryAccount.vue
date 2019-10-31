0af7e32b3485aabde6f7e54166dbee43f5f0905f
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-31 10:44:26 -0400

    minor modification for recovery account views

--- src/views/Auth/RecoveryAccount.vue
+++ src/views/Auth/RecoveryAccount.vue
@@ -6,24 +6,34 @@
           <v-col cols="6">
             <v-img :src="materialImage" width="100%" height="100%"></v-img>
           </v-col>
-          <v-col cols="6">
-            <v-form ref="loginForm">
+          <v-col cols="6" v-if="recovered">
+            <p class="text-center mt-4 mb-4">
+              <v-icon size="70" color="success">fa-check</v-icon>
+            </p>
+            <p class="font-weight-bold">{{$t('auth.accountRecovery.confirmRecoveryInformation')}}</p>
+          </v-col>
+          <v-col cols="6" v-else>
+            <v-form ref="accountRecoveryForm">
               <h1 class="text-center">{{$t('auth.recoveryAccount')}}</h1>
-              <p class="mt-3 mb-3">
-                {{$t('auth.recoveryAccountInformation')}}
-              </p>
+              <p class="mt-3 mb-3">{{$t('auth.recoveryAccountInformation')}}</p>
               <div class="mt-10 mb-3">
                 <v-text-field
+                  v-model="email"
                   name="email"
                   :label="$t('auth.email')"
                   id="email"
                   type="email"
                   placeholder="jhondoe@domain.com"
+                  :rules="rules.email"
                   outlined
                 ></v-text-field>
               </div>
               <div class="text-center">
-                <v-btn color="primary">{{$t('auth.recoveryAccount')}}</v-btn>
+                <v-btn
+                  @click="recovery()"
+                  :loading="loading"
+                  color="primary"
+                >{{$t('auth.recoveryAccount')}}</v-btn>
               </div>
             </v-form>
           </v-col>
@@ -33,11 +43,52 @@
   </div>
 </template>
 <script>
+import formRules from "@/mixins/Miscellany/FormRules";
+
 export default {
+  mixins: [formRules],
   data() {
     return {
-      materialImage: require("@/assets/images/MaterialData.png")
+      email: "",
+      materialImage: require("@/assets/images/MaterialData.png"),
+      loading: false,
+      recovered: false
     };
+  },
+  methods: {
+    recovery() {
+      if (!this.$refs.accountRecoveryForm.validate()) return false;
+
+      this.loading = true;
+
+      this.$store
+        .dispatch("auth/recoveryAccount", { email: this.email })
+        .then(response => {
+          setTimeout(() => {
+            this.recovered = true;
+            this.loading = false;
+          }, 1000);
+        })
+        .catch(error => {
+          console.log(error);
+          this.loading = false;
+          if (error.response.status === 409) {
+            this.$store.commit("snackbar/setSnackbar", {
+              show: true,
+              message: error.response.data,
+              color: "error",
+              bottom: true
+            });
+          } else {
+            this.$store.commit("snackbar/setSnackbar", {
+              show: true,
+              message: this.$t('server.serverError'),
+              color: "error",
+              bottom: true
+            });
+          }
+        });
+    }
   }
 };
 </script>
--- src/views/Auth/ResetPassword.vue
+++ src/views/Auth/ResetPassword.vue
@@ -6,32 +6,47 @@
           <v-col cols="6">
             <v-img :src="materialImage" width="100%" height="100%"></v-img>
           </v-col>
-          <v-col cols="6">
-            <v-form ref="loginForm">
+          <v-col cols="6" v-if="passwordResetConfirm">
+            <p class="text-center">
+              <v-icon color="success" size="70">fa-check</v-icon>
+            </p>
+            <p class="text-center">
+              {{$t('auth.accountRecovery.passwordResetSuccess')}}. {{$t('callAction.goto')}}
+              <router-link to="/auth/login">{{$t('auth.login')}}</router-link>
+            </p>
+          </v-col>
+          <v-col cols="6" v-else>
+            <v-form ref="resetPasswordForm">
               <h1 class="text-center">{{$t('auth.recoveryAccount')}}</h1>
-              <p class="mt-3 mb-3">
-                {{$t('auth.newPassword')}}
-              </p>
+              <p class="mt-3 mb-3">{{$t('auth.newPassword')}}</p>
               <div class="mt-10 mb-3">
                 <v-text-field
+                  v-model="password"
                   name="password"
                   :label="$t('auth.password')"
                   id="password"
-                  type="password"
+                  :type="showPassword ? 'text' : 'password'"
+                  :append-icon="showPassword ? 'fa-eye-slash' : 'fa-eye'"
+                  @click:append="showPassword = !showPassword"
                   placeholder="*********"
                   outlined
+                  :rules="rules.minLength8"
                 ></v-text-field>
                 <v-text-field
+                  v-model="confirmPassword"
                   name="confirmPassword"
                   :label="$t('auth.confirmPassword')"
                   id="confirmPassword"
-                  type="password"
+                  :type="showPassword ? 'text' : 'password'"
+                  :append-icon="showPassword ? 'fa-eye-slash' : 'fa-eye'"
+                  @click:append="showPassword = !showPassword"
                   placeholder="*********"
                   outlined
+                  :rules="rules.minLength8"
                 ></v-text-field>
               </div>
               <div class="text-center">
-                <v-btn color="success">{{$t('auth.recoveryAccount')}}</v-btn>
+                <v-btn color="success" :loading="loading" @click="resertPassword">{{$t('auth.recoveryAccount')}}</v-btn>
               </div>
             </v-form>
           </v-col>
@@ -41,16 +56,59 @@
   </div>
 </template>
 <script>
+import formRules from "@/mixins/Miscellany/FormRules";
+
 export default {
+  mixins: [formRules],
   mounted() {
-    if(this.$route.query.token === undefined){
-      this.$router.push('/auth/login');
+    if (this.$route.query.token === undefined) {
+      this.$router.push("/auth/login");
     }
   },
   data() {
     return {
-      materialImage: require("@/assets/images/MaterialData.png")
+      materialImage: require("@/assets/images/MaterialData.png"),
+      password: "",
+      confirmPassword: "",
+      showPassword: false,
+      loading: false,
+      passwordResetConfirm: false
     };
+  },
+  methods: {
+    resertPassword() {
+      if (!this.$refs.resetPasswordForm.validate()) {
+        return false;
+      }
+
+      this.loading = true;
+      const data = {
+        token: this.$route.query.token,
+        password: this.password,
+        confirm_password: this.confirmPassword
+      };
+      this.$store
+        .dispatch("auth/resetPassword", data)
+        .then(response => {
+          setTimeout(() => {
+            this.passwordResetConfirm = true;
+            this.loading = false;
+          }, 1000);
+        })
+        .catch(error => {
+          console.log(error);
+
+          if (error.response.status === 401) {
+            this.loading = false;
+            this.$store.commit("snackbar/setSnackbar", {
+              show: true,
+              message: this.$t('auth.accountRecovery.invalidToken'),
+              color: "error",
+              bottom: true
+            });
+          }
+        });
+    }
   }
 };
 </script>
